d00ad9cb89b4f75e097225a4a6eb380e2acbe96b
popstar724 <135359534+popstar724@users.noreply.github.com>
2023-07-23 19:10:49 +0800

    features table completed

---
+++ src/components/Table/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import FeatureTable from "./FeatureTheme";
+
+const TableIndex = () => {
+    return (
+        <div>
+            <FeatureTable />
+        </div>
+    )
+}
+
+export default TableIndex
